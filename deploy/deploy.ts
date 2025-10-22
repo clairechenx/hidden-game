79ca3f23412dc5997bd1722e5d3180544f13b85a
clairechenx <ipdjsac9974@hotmail.com>
2025-10-22 17:12:05 +0800

    update deploy.ts

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,17 +1,40 @@
+import * as dotenv from "dotenv";
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 
+dotenv.config();
+
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
-  const { deploy } = hre.deployments;
+  const { deploy, execute } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const infuraKey = process.env.INFURA_API_KEY;
+  if (!infuraKey) {
+    console.warn("INFURA_API_KEY is not set in environment variables.");
+  }
+
+  const coinDeployment = await deploy("ERC7984Coin", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  const gameDeployment = await deploy("TaskGame", {
+    from: deployer,
+    args: [coinDeployment.address],
+    log: true,
+  });
+
+  await execute(
+    "ERC7984Coin",
+    { from: deployer, log: true },
+    "setMinter",
+    gameDeployment.address,
+  );
+
+  console.log(`TaskGame deployed at ${gameDeployment.address}`);
+  console.log(`ERC7984Coin deployed at ${coinDeployment.address}`);
 };
+
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_task_game";
+func.tags = ["TaskGame", "ERC7984Coin"];
